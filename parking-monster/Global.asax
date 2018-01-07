c039395823d8825d37f234a5a2b10bc6302a3ef2
james.manglinong <james.manglinong@rcggs.com>
2018-01-07 18:58:46 +0800

    Add project files.

---
+++ parking-monster/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="parking_monster.MvcApplication" Language="C#" %>
---
+++ parking-monster/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(parking_monster.Startup))]
+namespace parking_monster
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
